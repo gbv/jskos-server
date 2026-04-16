bb446299015a6f9f76aa3e9148744ce3fcd8b664
Jakob Voss <voss@gbv.de>
2026-04-16 13:34:43 +0200

    Remove retry of database connection

--- utils/db.js
+++ utils/db.js
@@ -20,36 +20,16 @@ export function createDatabase(config) {
     config.warn("Disconnected from database, waiting for automatic reconnect...")
   }
 
-  async function connect(retry = false) {
+  async function connect() {
     connection.on("disconnected", onDisconnected)
-    function addErrorHandler() {
-      connection.on("error", (error) => {
-        config.error("Database error", error)
-      })
-    }
-    // If retry === false, add error handler before connecting
-    !retry && addErrorHandler()
+    connection.on("error", (error) => {
+      config.error("Database error", error)
+    })
     async function _connect() {
       const mongoUri = process.env.MONGO_URI || `${config.mongo.url}/${config.mongo.db}`
       return await mongoose.connect(mongoUri, config.mongo.options)
     }
-    let result
-    while (!result) {
-      try {
-        result = await _connect()
-      } catch (error) {
-        if (!retry) {
-          throw error
-        }
-        config.error(error)
-      }
-      if (!result) {
-        config.error("Error connecting to database, trying again in 10 seconds...")
-        await new Promise(resolve => setTimeout(resolve, 10000))
-      }
-    }
-    // If retry === true, add error handler after connecting
-    retry && addErrorHandler()
+    const result = await _connect()
     let collections, meta
     try {
       // Check meta collection whether upgrade script is necessary.
